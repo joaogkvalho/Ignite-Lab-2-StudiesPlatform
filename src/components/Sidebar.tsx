db6ed2cabe837a49ec5f5a1a4978b0d30b56b58c
joaogkvalho <joaogkvalho@gmail.com>
2022-06-22 17:39:12 -0300

    aula02 - sidebar finished

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,51 @@
+import { gql, useQuery } from "@apollo/client"
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+        query MyQuery {
+            lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+                title
+                slug
+                id
+                lessonType
+                availableAt
+            }
+        }
+
+    `
+
+interface GetLessonsQueryReponse {
+    lessons: {
+        title: string
+        slug: string
+        availableAt: string
+        id: string
+        lessonType: 'class' | 'live'
+    }[]
+}
+
 export function Sidebar(){
-    return(
+    const { data } = useQuery<GetLessonsQueryReponse>(GET_LESSONS_QUERY)
+    console.log(data)
+
+    return(        
         <aside className="w-[332px] p-5 bg-gray-700 border-l border-gray-600">
             <span className="font-bold text-xl pb-5 mb-5 border-b border-gray-600 block"> 
                 Cronograma de aulas
             </span>
 
             <div className="flex flex-col gap-8">
-                <Lesson /> 
-                <Lesson />
-                <Lesson />
-                <Lesson />
-                <Lesson />
+                {data?.lessons.map(lesson => {
+                    return (
+                        <Lesson
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            availableAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        /> 
+                    )
+                })}
             </div>
         </aside>
     )
