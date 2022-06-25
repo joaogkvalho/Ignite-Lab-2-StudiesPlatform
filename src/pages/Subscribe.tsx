f73f83d490ac3c8f142c4a12a1a638a30089d100
joaogkvalho <joaogkvalho@gmail.com>
2022-06-25 12:22:27 -0300

    subscriber page changed

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,4 +1,4 @@
-import { gql, useMutation } from "@apollo/client";
+import { Calendar, GithubLogo, Laptop } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
@@ -26,8 +26,8 @@ export function Subscribe(){
     }
 
     return(
-        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center justify-center">
+            <div className="w-screen h-screen max-w-[1100px] bg-reactLogo bg-no-repeat bg-center flex items-center justify-between mx-auto">
                 <div className="max-w-[624px]">
                     <Logo />
 
@@ -40,6 +40,45 @@ export function Subscribe(){
                         Em apenas uma semana você vai dominar na prática uma das tecnologias 
                         mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
+
+                    <div className="my-6 flex">
+                        <div className="flex mr-6">
+                            <Calendar size="48" />
+                            <span className="ml-2">
+                                Dos dias 20 <br/>
+                                a 26 de Junho
+                            </span>
+                        </div>
+
+                        <div className="flex">
+                            <Laptop size="48" />
+                            <span className="ml-2">
+                                Totalmente online<br/>
+                                e gratuito
+                            </span>
+                        </div>
+                    </div>
+                        
+                    <div className=" mt-12">
+                        <h1 className="text-xl text-bold">
+                            Professor Ministrante:
+                        </h1>
+                        <div className="flex items-center gap-4 mt-2">
+                            <img 
+                                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                src="https://github.com/joaogkvalho.png" 
+                                alt="João Gabriel"
+                            />
+                            <div className="leading-relaxed">
+                                <strong className="font-bold text-xl block">
+                                    João Gabriel
+                                </strong>
+                                <span className="text-gray-200 text-sm block">
+                                    Desenvolvedor Front-end | ReactJS
+                                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded">
@@ -67,19 +106,19 @@ export function Subscribe(){
                         >
                             Garantir minha vaga
                         </button>
+
+                        <div className="text-center text-xs text-gray-300">
+                            OU
+                        </div>
+
+                        <a href="#" 
+                            className="bg-gray-100 hover:bg-gray-300 gap-2 uppercase py-4 flex items-center justify-center rounded font-bold text-sm text-gray-900 transition-colors disabled:opacity-50"
+                        >   
+                            <GithubLogo size="24" />
+                            Fazer login com Github
+                        </a>   
                     </form>
                 </div>
-            </div>
-            <img src="/src/assets/image-mockup.png" className="mt-4" />
-
-            <div className="w-full px-12 py-6 bg-gray-900 border-t border-gray-600 flex items-center justify-between">
-                <p className="text-sm text-gray-300">
-                    João Gabriel - Todos os direitos reservados
-                </p>
-
-                <p className="text-sm text-gray-300">
-                    Políticas de privacidade
-                </p>
             </div>
         </div>
     )
